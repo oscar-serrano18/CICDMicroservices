4876d5d9c96522984edffc12f014837e6aea6082
oscar <oscars18@gmail.com>
2024-11-28 09:33:47 -0500

    upload microservices

---
+++ MicroserviceConfig/src/test/java/com/bancolombia/app/MicroserviceConfigApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bancolombia.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootTest
+class MicroserviceConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
